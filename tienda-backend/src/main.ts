a20abda88497a534d68bbeb994db1675a20fc4d1
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-01 21:12:00 +0000

    Add Swagger integration and enforce unique constraint on Product name

--- tienda-backend/src/main.ts
+++ tienda-backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder() // coumentacion de swagger
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
+
+  app.enableCors(); // Habilitar CORS para permitir solicitudes desde otros dominios
 }
 bootstrap();
