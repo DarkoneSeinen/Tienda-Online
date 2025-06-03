9cb67fd67080c771ce2fdc5e73fca26c63f6ab44
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-03 22:07:12 +0000

    Refactor product detail page to use Card component for improved layout and styling

--- tienda-frontend/src/app/products/[id]/page.tsx
+++ tienda-frontend/src/app/products/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { buttonVariants } from "@/components/ui/button";
 import { getProduct } from "../products.api";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
 
 interface Props {
     params: {
@@ -7,14 +11,33 @@ interface Props {
 }
 
 async function ProductDetailPage({params}: Props) {
-    console.log(params)
-    const product = await getProduct(params.id)
-    return (
-        <div>
-            <h1>Product Detail Page</h1>
-            <p>This is where the product details will be displayed.</p>
-        </div>
-    );
+    const product = await getProduct(params.id);
+    console.log(product);
+
+    return <div>
+
+        <Card>
+        <CardHeader>
+            <CardTitle className="flex justify-between">
+                Product Detail: {product.id}
+                <Link
+                    className={buttonVariants()}
+                    href="/"
+                >
+                    Go back
+                </Link>
+            </CardTitle>
+        </CardHeader>
+        <CardContent>
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt="" 
+                className="w-full h-64 object-cover"
+            />
+        </CardContent>
+    </Card>
+    </div>;
 }
 
 export default ProductDetailPage;
